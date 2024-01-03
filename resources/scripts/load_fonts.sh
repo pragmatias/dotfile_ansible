e30b34341a9e1f4971dfecc0f66ebdaffc9a5371
Pragmatias <pragmatias@gmail.com>
2024-01-03 19:04:56 +0100

    update

---
+++ resources/scripts/load_fonts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# clone
+mkdir -p /tmp/load_fonts
+
+cd /tmp/load_fonts
+
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/FiraCode.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/FiraMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/Hack.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/Mononoki.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/Noto.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/RobotoMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/Ubuntu.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/UbuntuMono.zip
+
+# Install fonts
+ls *.zip | xargs -l unzip -o -d ./tmp
+sudo cp ./tmp/*.ttf /usr/share/fonts/.
+rm -rf ./tmp
+
+
